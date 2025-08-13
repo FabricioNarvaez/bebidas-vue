29097edbadf3204b8907dd1daa68a16a8aae9573
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-13 12:15:10 +0200

    Add dynamic bg on header and show form only on home page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="bg-slate-800">
+    <header class="bg-slate-800" :class="{'headerBGImage': paginaInicio}">
         <div class="mx-auto container px-5 py-4">
             <div class="flex justify-between items-center">
                 <div>
@@ -16,7 +16,7 @@
                 </nav>
             </div>
 
-            <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <form v-if="paginaInicio" class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
                 <div class="space-y-4">
                     <label class="block text-white uppercase font-extrabold text-lg"
                         for="ingrediente">
@@ -44,3 +44,19 @@
         </div>
     </header>
 </template>
+
+<script setup>
+    import { computed } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    const paginaInicio = computed(() => route.name === 'inicio');
+</script>
+
+<style>
+    .headerBGImage {
+        background-image: url('/img/bg.jpg');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
