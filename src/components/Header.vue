a7413bd0bb2897c380f8013c66f5cabd69dbadc9
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-26 12:28:28 +0200

    Service to obtain categories created and implemented

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@
                 </nav>
             </div>
 
-            <form v-if="paginaInicio" class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <form v-if="paginaInicio" class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6" @submit.prevent="handleSubmit">
                 <div class="space-y-4">
                     <label class="block text-white uppercase font-extrabold text-lg"
                         for="ingrediente">
@@ -56,6 +56,9 @@
     const store = useBebidasStore();
     
     const paginaInicio = computed(() => route.name === 'inicio');
+    const handleSubmit = () => {
+        store.obtenerRecetas();
+    }
 </script>
 
 <style>
