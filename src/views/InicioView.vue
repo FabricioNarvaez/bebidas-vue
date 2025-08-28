15a82c946dea744bf5679708ae4a25726b6addbd
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-28 22:30:27 +0200

    Fix styles on InicioView 'recetas' container

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class="text-6xl font-extrabold">Recetas</h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 -gap-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
         <Receta v-for="receta in bebidasStore.recetas" :receta="receta" :key="receta.idDrink" />
     </div>
 </template>
