49afafe9abdf2480c78189805b3aaed95fdd526b
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-29 13:07:12 +0200

    Style Recipe component with border, shadow, and image enhancements

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        <h2>{{ receta.strDrink }}</h2>
+    <div class="border shadow-lg">
+        <div class="overflow-hidden">
+            <img class="hover:scale-105 transition-all hover:rotate-1"
+            :src="receta.strDrinkThumb" :alt="`Imagen de ${receta.strDrink}`">
+        </div>
+        
+        <div class="p-5">
+            <h2 class="text-2xl truncate font-bold">{{ receta.strDrink }}</h2>
+            <button class="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+                type="button">
+                Ver Receta
+            </button>
+        </div>
     </div>
 </template>
 
