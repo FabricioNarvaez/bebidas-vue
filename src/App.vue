09734de8ef3b6733d9beca4ea8579b27784e3a0e
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-09-10 11:18:04 +0200

    Create modal component with headless ui

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
     <main class="container mx-auto mt-10">
         <RouterView />
     </main>
+    <Modal/>
 </template>
 
 <script setup>
     import Header from './components/Header.vue';
+    import Modal from './components/Modal.vue';
 </script>
