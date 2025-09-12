ec9e956c4c40faa53b4505364904805773e79fdc
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-09-12 12:29:06 +0200

    Refactor Notificacion component to use store method for closing notifications

--- src/components/Notificacion.vue
+++ src/components/Notificacion.vue
@@ -18,7 +18,7 @@
                   </p>
                 </div>
                 <div class="ml-4 flex flex-shrink-0">
-                  <button type="button" @click="show = false" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  <button type="button" @click="notificacionStore.setShowFalse" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                     <span class="sr-only">Close</span>
                     <XMarkIcon class="h-5 w-5" aria-hidden="true" />
                   </button>
--- src/stores/notificacion.js
+++ src/stores/notificacion.js
@@ -6,9 +6,14 @@ export const useNotificacionStore = defineStore('notificacion', ()=>{
     const show = ref(false);
     const error = ref(false);
 
+    const setShowFalse = () => {
+        show.value = false;
+    }
+
     return {
         texto,
         show,
         error,
+        setShowFalse
     };
 });
