d5819d3b27dfb140a54c89a501113e56be0013d4
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-09-12 12:00:09 +0200

    Close modal after handleFavorito

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,10 +1,12 @@
 import { ref, watch, onMounted } from "vue";
 import { defineStore } from "pinia";
 import { useBebidasStore } from "./bebidas";
+import { useModalStore } from "./modal";
 
 export const useFavoritosStore = defineStore('favoritos', ()=>{
     const favoritos = ref([]);
     const bebidasStore = useBebidasStore();
+    const modalStore = useModalStore();
 
     watch(favoritos, ()=>{
         sincronizarLocalStorage();
@@ -22,9 +24,10 @@ export const useFavoritosStore = defineStore('favoritos', ()=>{
         const existeFavorito = favoritos.value.some(fav => fav.idDrink === bebidasStore.receta.idDrink);
         if(existeFavorito){
             favoritos.value = favoritos.value.filter(fav => fav.idDrink !== bebidasStore.receta.idDrink);
-            return;
-        };
-        favoritos.value.push(bebidasStore.receta);
+        }else{
+            favoritos.value.push(bebidasStore.receta);
+        }
+        modalStore.toggleModal();
     };
 
     const existeFavorito = (id) => {
